5ee65b72ae848be70b36d1687b5db02ee7eb410a
Boo <aikangtongxue@gmail.com>
2021-03-08 23:01:00 +0800

    style: 样式美化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,9 +3,23 @@
 <head>
 {{--  yield 的作用其实 更像是一个定一个变量，作为占位符  --}}
 {{--  第一个参数的作用是变量名称，第二个参数作用是默认值  --}}
-  <title>@yield('title', 'Weibo App') - Laravel 新手入门教程 </title>
+  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/">Weibo App</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+      <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+  @yield('content')
+</div>
 </body>
 </html>
